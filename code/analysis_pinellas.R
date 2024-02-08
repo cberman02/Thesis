0726d542b2f907aa51cc646e43aa91611ad9576c
cberman02 <cberman@bates.edu>
2024-02-08 11:49:36 -0500

    full detailed proposal

--- code/analysis_pinellas.R
+++ code/analysis_pinellas.R
@@ -7,8 +7,6 @@ ggplot(analysis_fl, aes(x = ridership/1000)) +
   geom_histogram(binwidth = 500, fill = "skyblue", color = "black", alpha = 0.7) +
   labs(title = "Ridership Histogram", x = "Ridership (in 1000s)", y = "Frequency")
 
-#Altamonte Springs serviced by Central Florida Regional Transportation Authority for bus and Central Florida Commuter Rail
-#for train
 print(unique(analysis_fl$agency))
 
 #Removing outliers
@@ -28,6 +26,7 @@ analysis_fl$time = ifelse(analysis_fl$date >= "2017-01-19", 1, 0) #Launch of Pin
 #analysis_fl$rail <- ifelse(analysis_fl$x3_mode == "Rail", 1, 0)
 
 #Regression for subsidy in Pinellas county
+#https://www.masstransitmag.com/alt-mobility/shared-mobility/car-sharing/press-release/12296934/pinellas-suncoast-transit-authority-psta-psta-brings-together-uber-and-taxi-to-get-people-on-the-bus
 didreg <- feols(
   fml = ridership ~ pop + white + med_house_income + poverty + treated + time + treated*time,
   vcov = "iid",
@@ -40,5 +39,8 @@ didreg_fe <- feols(
 )
 etable(
   didreg, didreg_fe, 
-  coefstat = "tstat", digits = 3, digits.stats = 3
+  coefstat = "tstat", digits = 3, digits.stats = 3,
+  tex = TRUE
 )
+
+write_csv(analysis_fl, "data/census_transit_fl.csv")
