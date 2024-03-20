9e6f30bb5c34098e86f42557466d1c858ec1a202
cberman02 <cberman@bates.edu>
2024-03-19 20:37:18 -0400

    Updated coef plot

--- code/graphs.R
+++ code/graphs.R
@@ -45,34 +45,37 @@ ggplot(uza_agency_counts, aes(x = unique_agencies)) +
 ggsave("images/avg_agencies_uza.png")
 
 # COEFFICIENT PLOT
-#Creating table of t test values
-t_test_df <- data.frame()
-for (i in 2005:2020) {
-  pre_covid_year <- pre_covid %>%
-    filter(year == i)
-  t_test_result <- t.test(log(ridership) ~ treated, pre_covid_year)
-  t_test_df <- rbind(t_test_df, data.frame(year = i, coefficient = t_test_result$estimate, 
-                                           lower_ci = t_test_result$conf.int[1], upper_ci = t_test_result$conf.int[2]))
-}
-#Bit of nonsense to remove duplicates and format the table nicely
-t_test_year <- t_test_df %>%
-  select(year, lower_ci, upper_ci) %>%
-  unique()
-t_test_df <- tibble::rowid_to_column(t_test_df, "group") %>%
-  mutate(group = ifelse(group %% 2 != 0, "control", "treated"))
-t_test_coeff <- t_test_df %>%
-  select(year, coefficient, group) %>%
-  pivot_wider(names_from = group, values_from = coefficient, names_prefix = "group")
-t_test_df <- merge(t_test_coeff, t_test_year, by = "year") %>%
-  rename(control = groupcontrol, treated = grouptreated)
-t_test_df$diff <- t_test_df$control-t_test_df$treated
-#xtable(t_test_df, type = "latex")
+#Running log(ridership) + treated*time
+pre_covid_abbr <- pre_covid %>%
+  filter(date >= "2016-01-01") 
+did_uza_ln <- feols(log(ridership) ~ i(date, treated, "2019-12-01") |+ ## Our key interaction: time × treatment status
+                    agency + date,                             ## FEs
+                    cluster = ~agency,                          ## Clustered SEs
+                    data = pre_covid_abbr)
+#iplot(did_uza_ln, 
+      #xlab = 'Time to treatment',
+      #main = 'Coefficient Plot') 
+pre_covid_abbr <- pre_covid_abbr %>%
+  filter(date != "2019-12-01") #filtering out date to prevent colinearity
+
+graph_df <- data.frame(date = unique(pre_covid_abbr$date),
+                       coef = did_uza_ln$coefficients,
+                       ci = did_uza_ln$se*1.96) %>%
+  mutate(date = as.Date(date)) #Creating df to make ggplot graph from
+ggplot(graph_df, aes(x = date, y = coef)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = coef-ci, ymax = coef+ci), width = 0.2) +
+  geom_vline(xintercept = as.Date("2019-12-01"), linetype = "dashed", color = "red") +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "blue") +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +  # Abbreviate x-axis labels to display only the year
+  labs(x = "Year", y = "Coefficient", title = "Coefficient Plot")
 
 # Plot
 ggplot(t_test_df, aes(x = factor(year))) +
   geom_point(aes(y = diff), color = "blue") +
   geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci), width = 0.2) +
   geom_hline(yintercept = 0, linetype = "dashed", color = "red") +  # Add a line at y = 0
+  geom_vline(xintercept = 2019, linetype = "dashed", color = "green") +
   labs(title = "Difference between Treated and Control Ridership",
        x = "Year",
        y = "Difference") +
@@ -115,4 +118,5 @@ ggplot(transit_rankings,aes(x = score)) +
   geom_vline(xintercept = mean(transit_rankings$score), color = "red") +
   geom_vline(xintercept = quantile(transit_rankings$score, c(0.125, 0.875), na.rm = TRUE), color = "red") +
   theme_minimal() 
+ggsave("images/avg_agency_score.png")
 
--- code/summary_stats.R
+++ code/summary_stats.R
@@ -4,6 +4,6 @@ latex_table <- stargazer(rankings, title = "Summary Statistics", type = "latex")
 
 #Ridership sum stats
 ridership <- read.csv("data/transit_data.csv")
-st(ridership, vars = c("ridership", "x3_mode"), 
+st(ridership, vars = c("ridership","total_ridership", "x3_mode"), 
    out = "latex")
    
